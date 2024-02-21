9dfecc1b911b2316f49eb196d62275f06ae7ac2f
yuvraj0702 <yuvraj272003@gmail.com>
2024-02-21 13:57:02 +0530

    added

---
+++ dsa/linked_list/2.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+typedef struct Node Node;
+
+void printList(Node* node) {
+    while (node != NULL) {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+int main() {
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+
+    head = (Node*)malloc(sizeof(Node));
+    second = (Node*)malloc(sizeof(Node));
+    third = (Node*)malloc(sizeof(Node));
+
+    
+    head->data = 100;
+    head->next = second;
+
+    second->data = 200;
+    second->next = third;
+
+    third->data = 300;
+    third->next = NULL;
+
+    
+    printf("Linked list elements: ");
+    printList(head);
+
+    
+    free(head);
+    free(second);
+    free(third);
+
+    return 0;
+}
