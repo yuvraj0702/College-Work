d55df933fe731c5e51dcd81b04c9b979fbfcfe3f
YUVRAJ PRATAP SINGH <144945455+yuvraj0702@users.noreply.github.com>
2024-02-27 12:07:58 +0530

    Update 6.py

--- python/class test python/6.py
+++ python/class test python/6.py
@@ -1,4 +1,3 @@
-
 def euclidean_distance(point1, point2):
     distance = ((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2) ** 0.5
     return distance
@@ -15,7 +14,7 @@ def angle_between_lines(point1, point2, point3, point4):
 def is_regular_quadrilateral(distances):
     return all(distance == distances[0] for distance in distances)
 
-if __name__ == "__main__":
+def main():
     # Define four points
     point1 = (1, 1)
     point2 = (4, 5)
@@ -45,3 +44,6 @@ if __name__ == "__main__":
         print("The quadrilateral is regular.")
     else:
         print("The quadrilateral is not regular.")
+
+
+main()
