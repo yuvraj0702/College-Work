54776e7db6bff73e2a46f39f2cfe1e46b6159639
yuvraj0702 <yuvraj272003@gmail.com>
2024-04-08 15:58:54 +0530

    added

---
+++ python/python29.py
@@ -0,0 +1,16 @@
+class room:
+    length:0.0
+    breadth:0.0
+    
+    def calculate_area(self):
+        print("area of room =",self.length*self.breadth)
+       
+       
+study_room=room()
+study_room.length=42.5
+study_room.breadth=30.8
+        
+        
+        
+study_room.calculate_area()
+        
